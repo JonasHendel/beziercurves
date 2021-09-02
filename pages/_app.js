1130da053793cbb7207b9e7cec81294c646be996
JonasHendel <70843626+JonasHendel@users.noreply.github.com>
2021-09-02 20:44:12 +0200

    lr

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,12 @@
 import '../styles/globals.css'
+import LogRocket from 'logrocket';
 
 function MyApp({ Component, pageProps }) {
+  LogRocket.init('xfw64p/beziercurves');
+  LogRocket.identify('user', {
+    name: 'user',
+    email: 'user@gmail.com'
+  });
   return <Component {...pageProps} />
 }
 
